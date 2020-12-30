3fceb31c2d31ab523c5d3e2e854b15c3513caf17
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2020-12-30 17:14:22 -0600

    Basic About me

--- Front-End/src/Components/About/About.js
+++ Front-End/src/Components/About/About.js
@@ -5,7 +5,19 @@ import '../../Containers/Terminal/Terminal.css'
 const About = () =>{
     return(
     <div className = "main">
-        <h1>About Me</h1>
+        <h1><b className="I">[1]</b> About Me<br/>⠀</h1>
+        <p>Hello! I'm Iliyan, a third year student at Colorado State University studying Computer Science with a focus in Software Engineering<br/>⠀</p>
+        <p>Here's some technologies that I've worked with:</p>
+        <ol>
+            <li>Java</li>
+            <li>Python</li>
+            <li>C++</li>
+            <li>React</li>
+            <li>Angular</li>
+            <li>Vue</li>
+            <li>Node.js</li>
+            <li>REST Archetecture</li>
+        </ol>
     </div>
     );
 }
