c8868e8d852f6acb629fd5ea73dd98bc1e8e1124
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2021-01-02 22:02:18 -0700

    Updated About me

--- Front-End/src/Components/About/About.js
+++ Front-End/src/Components/About/About.js
@@ -4,22 +4,30 @@ import '../../Containers/Terminal/Terminal.css'
 import avatar from '../../Resources/avatar.jpg'
 const About = () =>{
     return(
-    <div className = "main">
-        <div  className="header">
-            <h1><b className="I">[1]</b> About Me</h1>
+    <section className = "main section">
+        <div className="about">
+            <div className="text">
+                <div  className="header">
+                    <h1>About Me</h1>
+                </div>
+                <p>Hello! I'm Iliyan, an aspiring software engineer based in Fort Collins, CO<br/>⠀</p>
+                <p>Currently I'm a third year student at <a href="https://compsci.colostate.edu/" target="_blank" rel="noreferrer">Colorado State University</a>. 
+                    When I'm not coding or studying you'll find me running or snowboarding.
+                <br/>⠀
+                </p>
+                <p>Here are a few technologies I've worked with recently:<br/>⠀</p>
+                <ol className="technologies">
+                    <li>HTML & (S)CSS</li>
+                    <li>JavaScript (ES10+)</li>
+                    <li>React</li>
+                    <li>Vue</li>
+                    <li>Angular</li>
+                    <li>Wordpress</li>
+                </ol>
+            </div>
+            <div className="avatar"></div>
         </div>
-        <p>Hello! I'm Iliyan, a third year student at Colorado State University studying Computer Science with a focus in Software Engineering<br/>⠀</p>
-        <img className="avatar" src={avatar} alt="avatar"/>
-        <p>Here's some technologies that I've worked with:</p>
-        <ol>
-            <li>HTML & (S)CSS</li>
-            <li>Javascript (ES6+)</li>
-            <li>React.js</li>
-            <li>Vue</li>
-            <li>Angular</li>
-            <li>Workpress</li>
-        </ol>
-    </div>
+    </section>
     );
 }
 export default About;
